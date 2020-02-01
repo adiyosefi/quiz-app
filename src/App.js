8f0bb465dfefdeaecb7dd583ef3c6cccdfdfbaf1
Adi Nomberg <adinomberg6@gmail.com>
2020-02-01 20:02:50 +0200

    questions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,175 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { QUESTIONS } from "./questions";
+
+
+const Answer = ({ answer, currentQuestion, setQuestions, questions, question, userAnswers, setUserAnswers }) => {
+
+    const handleAnswerChange = (e) => {
+        // setQuestions(questions.map((q, i) => {
+        //     if (q.id === currentQuestion){
+        //         q.user_ans_index = e.target.value;
+        //     }
+        // }));
+        // (userAnswers.map((userAnswer, index) => {
+        //     if (question[currentQuestion]) {
+        //         userAnswer = e.target.value;
+        //     }
+        // }));
+
+        console.log(userAnswers);
+    }
+
+
+    return (
+        <div>
+            <input name={question.question_content} type="radio" id={answer.ans_content} value={answer.ans_id}
+                   onChange={e => handleAnswerChange(e)}/>
+            <label htmlFor={answer.ans_content}>{answer.ans_content}</label>
+        </div>
+    );
+};
+
+
+const RenderAnswers = ({ answers, currentQuestion, setQuestions, questions, question, userAnswers, setUserAnswers}) => {
+
+    const renderAnswers = answers.map((answer, index) => {
+        return (
+            <li key={answer.ans_id}>
+                <Answer answer={answer} currentQuestion={currentQuestion} setQuestions={setQuestions} questions={questions} question={question} userAnswers={userAnswers} setUserAnswers={setUserAnswers}/>
+            </li>
+        );
+    });
+
+    if (answers != null) {
+        return (
+            <div>
+                <form>
+                {renderAnswers}
+                </form>
+            </div>
+        );
+    } else {
+        return (
+            <div></div>
+        );
+    }
+};
+
+const Question = ({ questions, currentQuestion, setQuestions, setCurrentQuestion, userAnswers, setUserAnswers }) => {
+
+    const questionItem = questions.map((question, index) => {
+        if (question.id === currentQuestion) {
+            return (
+                <div key={question.id}>
+                    <div>
+                        <h4>{question.question_content}</h4>
+                    </div>
+                        <ul>
+                            <RenderAnswers answers={question.answers} currentQuestion={currentQuestion} question={question}
+                                           userAnswers={userAnswers} setQuestions={setQuestions} questions={questions}
+                                           setUserAnswers={setUserAnswers}/>
+                        </ul>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    });
+    if (questions != null) {
+        return (
+            <div>
+                {questionItem}
+            </div>
+        );
+    } else {
+        return (
+            <div></div>
+        );
+    }
+};
+
+const PrevButton = ({ questions , currentQuestion, setCurrentQuestion }) => {
+    const goToPrev = () => {
+        if (currentQuestion > 0){
+            setCurrentQuestion(currentQuestion-1);
+        }
+    };
+
+    if (currentQuestion >= 0 && currentQuestion <= questions.length-1) {
+        return (
+            <button type="button" disabled={currentQuestion === 0 ? true : false} onClick={goToPrev}>Prev</button>
+        );
+    }
+
+}
+
+const NextButton = ({ questions , currentQuestion, setCurrentQuestion }) => {
+    const goToNext = () => {
+        if (currentQuestion < questions.length){
+            setCurrentQuestion(currentQuestion+1);
+        }
+    };
+
+    if (currentQuestion >= 0 && currentQuestion <= questions.length-1) {
+        return (
+            <button type="button" disabled={currentQuestion === questions.length-1 ? true : false} onClick={goToNext}>Next</button>
+        );
+    }
+
+}
+
+const DoneButton = ({ questions , currentQuestion, setCurrentQuestion }) => {
+    if (currentQuestion === questions.length-1) {
+        return (
+            <button type="submit">Done</button>
+        );
+    }
+    return (
+        <span></span>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [questions, setQuestions] = useState( QUESTIONS );
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const [userAnswers, setUserAnswers] = useState( [] );
+
+
+    const handleClick = () => {
+        if (userAnswers[currentQuestion] != null) {
+            setQuestions([
+                ...questions,
+                questions[currentQuestion].user_ans_index=userAnswers[currentQuestion]
+            ]);
+            setUserAnswers([]);
+        }
+    };
+
+    return (
+        <div className="container">
+            <div>
+                <h1>Quiz Time!</h1>
+            </div>
+            {/*<form onSubmit={(e) => {*/}
+            {/*    e.preventDefault();*/}
+            {/*    handleClick();*/}
+            {/*}}>*/}
+            <Question questions={questions} currentQuestion={currentQuestion} setQuestions={setQuestions}
+                      setCurrentQuestion={setCurrentQuestion} userAnswers={userAnswers} setUserAnswers={setUserAnswers}/>
+            <div>
+                <PrevButton questions={questions} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion}/>
+                <NextButton questions={questions} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion}/>
+                <DoneButton questions={questions} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion}/>
+            </div>
+            {/*</form>*/}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/questions.js
@@ -0,0 +1,42 @@
+export const QUESTIONS = [
+    {
+        id: 0,
+        question_content: 'What is the capital of England?',
+        answers: [{ans_id: 0, ans_content: 'London'}, {ans_id: 1, ans_content: 'Tel Aviv'},
+            {ans_id: 2, ans_content: 'Paris'}, {ans_id: 3, ans_content: 'Los Angeles'} ],
+        correct_ans_index: 0,
+        user_ans_index: null
+    },
+    {
+        id: 1,
+        question_content: 'What is the best name ever?',
+        answers: [{ans_id: 0, ans_content: 'Shira'}, {ans_id: 1, ans_content: 'Adi'},
+            {ans_id: 2, ans_content: 'Amit'}, {ans_id: 3, ans_content: 'Dana'} ],
+        correct_ans_index: 1,
+        user_ans_index: null
+    },
+    {
+        id: 2,
+        question_content: 'What is the name of the Chinese virus??',
+        answers: [{ans_id: 0, ans_content: 'Tuburg'}, {ans_id: 1, ans_content: 'Goldstar'},
+            {ans_id: 2, ans_content: 'Corona'}, {ans_id: 3, ans_content: 'Fanta'} ],
+        correct_ans_index: 2,
+        user_ans_index: null
+    },
+    {
+        id: 3,
+        question_content: 'Which continent Israel belongs to?',
+        answers: [{ans_id: 0, ans_content: 'Europe'}, {ans_id: 1, ans_content: 'Australia'},
+            {ans_id: 2, ans_content: 'America'}, {ans_id: 3, ans_content: 'Asia'} ],
+        correct_ans_index: 3,
+        user_ans_index: null
+    },
+    {
+        id: 4,
+        question_content: 'How many letters in the word BLA?',
+        answers: [{ans_id: 0, ans_content: '4'}, {ans_id: 1, ans_content: '2'},
+            {ans_id: 2, ans_content: '6'}, {ans_id: 3, ans_content: '3'} ],
+        correct_ans_index: 3,
+        user_ans_index: null
+    }
+];
